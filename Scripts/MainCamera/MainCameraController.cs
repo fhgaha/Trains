93f7048d6d944f2a4b0f5289f0ea5b7ba81123a5
fhgaha <antsy1992@gmail.com>
2022-05-15 01:48:00 +0300

    camera zoom

--- Scripts/MainCamera/MainCameraController.cs
+++ Scripts/MainCamera/MainCameraController.cs
@@ -5,6 +5,10 @@ namespace Trains.Scripts.MainCamera
 {
 	public class MainCameraController : Spatial
 	{
+		////////////////////////////////
+		//export
+		////////////////////////////////
+		//movement
 		[Export(PropertyHint.Range, "0,1000,")]
 		public float MovementSpeed { get; set; } = 20f;
 
@@ -18,39 +22,67 @@ namespace Trains.Scripts.MainCamera
 		[Export(PropertyHint.Range, "0,1000,0.1")]
 		public float RotationSpeed { get; set; } = 10;
 
+		//zoom 
+		[Export(PropertyHint.Range, "0,500,")]
+		public float MinZoom { get; set; } = 0.5f;
+
+		[Export(PropertyHint.Range, "0,1000,")]
+		public float MaxZoom { get; set; } = 90f;
+
+		[Export(PropertyHint.Range, "0,1000,0.1")]
+		public float ZoomSpeed { get; set; } = 10f;
+
+		[Export(PropertyHint.Range, "0,1,0.1")]
+		public float ZoomSpeedDamp { get; set; } = 0.5f;
+
 		//flags
 		[Export]
 		public bool RotationAllowed { get; set; } = true;
 
 		[Export]
-		public bool InvertRotationHorizontal {get; set;} = false;
+		public bool InvertRotationHorizontal { get; set; } = false;
 
 		[Export]
 		public bool InvertRotationVertical { get; set; } = false;
 
+		////////////////////////////////
+		//params
+		////////////////////////////////
 		private Vector2 lastMousePos = new Vector2();
 		private bool isRotating = false;
 		private Spatial elevation;
+		private float zoomDirection = 0f;
+		private Camera camera;
 
+		////////////////////////////////
+		//methods
+		////////////////////////////////
 		public override void _Ready()
 		{
 			elevation = GetNode<Spatial>("Elevation");
+			camera = GetNode<Spatial>("Elevation").GetNode<Camera>("Camera");
 		}
 
 		public override void _Process(float delta)
 		{
 			Move(delta);
 			Rotate(delta);
+			Zoom(delta);
 		}
 
 		public override void _UnhandledInput(InputEvent @event)
 		{
+			//check if rotating is happening
 			if (@event.IsActionPressed("camera_rotate"))
 			{
 				isRotating = true;
 				lastMousePos = GetViewport().GetMousePosition();
 			}
 			if (@event.IsActionReleased("camera_rotate")) isRotating = false;
+
+			//check if zooming is happening
+			if (@event.IsActionPressed("camera_zoom_in")) zoomDirection = -1;
+			if (@event.IsActionPressed("camera_zoom_out")) zoomDirection = 1;
 		}
 
 		private void Move(float delta)
@@ -59,20 +91,12 @@ namespace Trains.Scripts.MainCamera
 
 			var velocity = new Vector3();
 
-			if (Input.IsActionPressed("camera_forward"))
-				velocity -= Transform.basis.z;
-
-			if (Input.IsActionPressed("camera_backwards"))
-				velocity += Transform.basis.z;
-
-			if (Input.IsActionPressed("camera_left"))
-				velocity -= Transform.basis.x;
-
-			if (Input.IsActionPressed("camera_right"))
-				velocity += Transform.basis.x;
+			if (Input.IsActionPressed("camera_forward")) velocity -= Transform.basis.z;
+			if (Input.IsActionPressed("camera_backwards")) velocity += Transform.basis.z;
+			if (Input.IsActionPressed("camera_left")) velocity -= Transform.basis.x;
+			if (Input.IsActionPressed("camera_right")) velocity += Transform.basis.x;
 
 			velocity = velocity.Normalized();
-
 			Translation += velocity * delta * MovementSpeed;
 		}
 
@@ -92,7 +116,6 @@ namespace Trains.Scripts.MainCamera
 			Vector2 currentMousePos = GetViewport().GetMousePosition();
 			Vector2 displacement = currentMousePos - lastMousePos;
 			lastMousePos = currentMousePos;
-			//GD.Print(displacement);
 			return displacement;
 		}
 
@@ -111,8 +134,33 @@ namespace Trains.Scripts.MainCamera
 			float invertCoeff = invert ? 1 : -1;
 			float newElevation = elevation.RotationDegrees.x + value * delta * RotationSpeed * invertCoeff;
 			newElevation = Mathf.Clamp(newElevation, -MaxElevationAngle, -MinElevationAngle);
-			elevation.RotationDegrees = new Vector3(
-				newElevation, elevation.RotationDegrees.y, elevation.RotationDegrees.z);
+			elevation.RotationDegrees
+				= new Vector3(newElevation, elevation.RotationDegrees.y, elevation.RotationDegrees.z);
+		}
+
+		private void Zoom(float delta)
+		{
+			float newZoom = camera.Translation.z + ZoomSpeed * delta * zoomDirection;
+			newZoom = Mathf.Clamp(newZoom, MinZoom, MaxZoom);
+			camera.Translate(new Vector3(0, 0, newZoom));
+			camera.Translation = new Vector3(camera.Translation.x, camera.Translation.y, newZoom);
+			//stop scrolling
+			zoomDirection *= ZoomSpeedDamp;
+			if (Mathf.Abs(zoomDirection) <= 0.0001f) zoomDirection = 0;
+			//ElevateUsingZoom(delta, InvertRotationVertical);
+		}
+		
+		//the idea is to rotate cam vertically on zoom like in paradox games or in railroad tycoon 3
+		private void ElevateUsingZoom(float delta, bool invert = false)
+		{
+			float invertCoeff = invert ? 1 : -1;
+			float newElevation = elevation.RotationDegrees.x 
+			- delta * RotationSpeed * invertCoeff / camera.Translation.z;
+			newElevation = Mathf.Clamp(newElevation, -MaxElevationAngle, -MinElevationAngle);
+			elevation.RotationDegrees
+				= new Vector3(newElevation, elevation.RotationDegrees.y, elevation.RotationDegrees.z);
+			
+			GD.Print("elev: " + elevation.RotationDegrees.x + ", zoom: " + camera.Translation.z );
 		}
 	}
 }
