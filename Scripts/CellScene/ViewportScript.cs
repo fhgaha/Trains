ad41c0b50c176b2e833dc86c8bc5148a4e253742
fhgaha <antsy1992@gmail.com>
2022-05-16 14:18:14 +0300

    cell label is displayed

---
+++ Scripts/CellScene/ViewportScript.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+namespace Trains.Scripts.CellScene
+{
+	[Tool]
+	public class ViewportScript : Viewport
+	{
+		Label label;
+
+		public override void _Ready()
+		{
+		    label = GetNode<Label>("Label");   
+		    Size = label.RectSize;
+		}
+
+		// public override void _Process(float delta)
+		// {
+		// 	this.Size = GetNode<Label>("Label").RectSize;
+		// }
+
+		public void SetText(string text)
+		{
+			label.Text = text;
+			Size = label.RectSize;
+		}
+	}
+}
