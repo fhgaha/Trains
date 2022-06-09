506bacf3c71e589e43ff7579b0bae0eb1d85ed24
fhgaha <antsy1992@gmail.com>
2022-06-09 21:47:44 +0300

    added trajectory calculation. game crashes.

--- Model/Builders/RailBuilder.cs
+++ Model/Builders/RailBuilder.cs
@@ -47,12 +47,26 @@ namespace Trains.Model.Builders
 		{
 			if (!(Global.MainButtonMode is MainButtonType.BuildRail)) return;
 			UpdateBlueprint();
+
+			if (!(firstSegment is null))
+			{
+				Vector2 end = GetViewport().GetMousePosition();
+				// be cautious
+				//converting vec2 to vec3 back and forth meh
+				var path = firstSegment.GetNode<Path>("Path");
+				Vector3 start3d = path.Curve.GetClosestPoint(new Vector3(end.x, 0, end.y));
+				Vector2 start = new Vector2(start3d.x, start3d.z);
+				var points = CalculateTrajectory(start, end, 50);
+				var curve = new Curve3D();
+				points.ForEach(p => curve.AddPoint(new Vector3(p.x, 0, p.y)));
+				path.Curve = curve;
+			}
 		}
 
 		public override void _UnhandledInput(InputEvent @event)
 		{
 			if (@event is InputEventMouseButton ev && ev.IsActionPressed("lmb"))
-				if (!(blueprint is null) && canBuild)
+				if (!(blueprint is null) && canBuild && firstSegment is null)
 					PlaceObject(blueprint.Translation, blueprint.Rotation);
 
 			if (!(blueprint is null) && @event.IsActionPressed("Rotate"))
@@ -68,6 +82,7 @@ namespace Trains.Model.Builders
 			rail.Rotation = rotation;
 			//rail.GetNode<CollisionPolygon>("Path/CSGPolygon/Area/CollisionPolygon").Disabled = false;
 			objectHolder.AddChild(rail);
+			firstSegment = rail;
 		}
 
 		private void UpdateBlueprint()
@@ -138,6 +153,37 @@ namespace Trains.Model.Builders
 			var collider = blueprint.GetNode<CollisionPolygon>("Path/CSGPolygon/Area/CollisionPolygon");
 			collider.Polygon = csg.Polygon;
 			AddChild(blueprint);
+		}
+
+		private List<Vector2> CalculateTrajectory(Vector2 startPos, Vector2 endPos, int numPoints)
+		{
+			float gravity = -9.8f;
+
+			var points = new List<Vector2>();
+			var startEndDir = (endPos - startPos).Normalized();
+			var DOT = Vector2.Right.Dot(startEndDir);   //-1, 0 or 1
+			var angle = 90 - 45 * DOT;
+
+			var xDist = endPos.x - startPos.x;
+			var yDist = -1f * (endPos.y - startPos.y);
+
+			var speed = Sqrt(
+				((0.5f * gravity * xDist * xDist) / Pow(Cos(Pi / 180 * angle), 2f))
+				/ (yDist - (Tan(Pi / 180 * angle) * xDist)));   
+			var xComp = Cos(Pi / 180 * angle) * speed;
+			var yComp = Sin(Pi / 180 * angle) * speed;
+
+			var totalTime = xDist / xComp;
+
+			for (int i = 0; i < numPoints; i++)
+			{
+				var time = totalTime * i / numPoints;
+				var dx = time * xComp;
+				var dy = -1f * (time * yComp + 0.5f * gravity * time * time);
+				points.Add(startPos + new Vector2(dx, dy));
+			}
+
+			return points;
 		}
 	}
 }
--- Model/Common/Extensions.cs
+++ Model/Common/Extensions.cs
@@ -12,5 +12,15 @@ namespace Trains.Model.Common
 				child.QueueFree();
 			}
 		}
+
+	// func vec2_vec3(vec3, axis):
+    // var array = [vec3.x, vec3.y, vec3.z]
+    // array.remove(axis)
+    // return Vector2(array[0], array[1])
+
+	// func vec3_vec2(vec2, axis, value):
+    // var array = [vec2.x, vec2.y]
+    // array.insert(axis, value)
+    // return Vector3(array[0], array[1], array[2])
 	}
 }
