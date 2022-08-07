53ccbd881899ad71cc034915fc179327d76306a8
fhgaha <antsy1992@gmail.com>
2022-08-07 18:02:11 +0300

    test railroad trains go back and forth

--- Scripts/PathFollow/MyPathFollow.cs
+++ Scripts/PathFollow/MyPathFollow.cs
@@ -7,9 +7,31 @@ namespace Trains
 	{
 		[Export] private float speed = 2;
 
-		public override void _Process(float delta)
+		[Export] private bool goForth = true;
+
+		public override void _PhysicsProcess(float delta)
 		{
-			Offset += speed * delta;
+			Offset += goForth
+				? speed * delta
+				: -speed * delta;
+
+			ChangeDirectionIfNeeded();
 		}
+
+		private void ChangeDirectionIfNeeded()
+		{
+			if (ReachedEnd())
+			{
+				goForth = false;
+			}
+			else if (ReachedStart())
+			{
+				goForth = true;
+			}
+		}
+
+		private bool ReachedStart() => UnitOffset == 0;
+
+		private bool ReachedEnd() => UnitOffset == 1;
 	}
 }
