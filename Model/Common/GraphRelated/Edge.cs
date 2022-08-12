59abe1ec2052def68b642a502f3ec3414f1232a1
fhgaha <antsy1992@gmail.com>
2022-08-12 16:50:36 +0300

    graph.cs, node.cs, edge.cs

---
+++ Model/Common/GraphRelated/Edge.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Trains.Model.Common
+{
+	public static partial class Dijkstra
+	{
+		public class Edge
+		{
+			public readonly Node From;
+			public readonly Node To;
+			public Edge(Node first, Node second)
+			{
+				this.From = first;
+				this.To = second;
+			}
+			public bool IsIncident(Node node)
+			{
+				return From == node || To == node;
+			}
+			public Node OtherNode(Node node)
+			{
+				if (!IsIncident(node)) throw new ArgumentException();
+				if (From == node) return To;
+				return From;
+			}
+		}
+	}
+}
