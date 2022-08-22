c7bcea28a775e5d7cf15384e8d7e3b135914b06c
fhgaha <antsy1992@gmail.com>
2022-08-22 12:57:34 +0300

    t TestCurveSegment

---
+++ Tests/TestCurveSegment.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace Trains.Tests
+{
+	[Start(nameof(RunBeforeTestClass))]
+	[Pre(nameof(RunBeforeTestMethod))]
+	[Post(nameof(RunAfterTestMethod))]
+	[End(nameof(RunAfterTestClass))]
+
+	public class TestCurveSegment : WAT.Test
+	{
+		//Developers may target a method with the [Start] attribute to execute code before any test method is run
+		public void RunBeforeTestClass() { }
+
+		// Developers may target a method with the [Pre] attribute to execute code before each test method is run;
+		public void RunBeforeTestMethod() { }
+
+		[Test]
+		public void BuildSegmentUsingVectorsTest()
+		{
+			var first = new Vector3(1, 0, 0);
+			var second = new Vector3(0, 1, 0);
+
+			var segment = new CurveSegment(first, second);
+
+			Assert.IsEqual(segment.First, first, "segment.First should contain first passed parameter");
+			Assert.IsEqual(segment.Second, second, "segment.Second should contain second passed parameter");
+		}
+
+		[Test]
+		public void BuildSegmentUsingCollectionTest()
+		{
+			var first = new Vector3(1, 0, 0);
+			var second = new Vector3(0, 1, 0);
+			var collection = new List<Vector3> { first, second };
+
+			var segment = new CurveSegment(collection);
+
+			Assert.IsEqual(segment.First, first, "segment.First should contain first passed parameter");
+			Assert.IsEqual(segment.Second, second, "segment.Second should contain second passed parameter");
+		}
+
+		// Developers may target a method with the [Post] attribute to execute code after each test method is run
+		public void RunAfterTestMethod() { }
+
+		// Developers may target a method with the [End] attribute to execute after all tests method have run
+		public void RunAfterTestClass() { }
+	}
+}
