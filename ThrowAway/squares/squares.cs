e1e638d29d683df966a8b9c78bf23fa722b791d7
fhgaha <antsy1992@gmail.com>
2023-03-05 03:08:27 +0300

    tested squares. turns out i can change whole 128x128 field colors for each cell separately in 60fps.

--- ThrowAway/squares/squares.cs
+++ ThrowAway/squares/squares.cs
@@ -6,9 +6,15 @@ public class squares : MultiMeshInstance
 	private Random _rnd;
 	private const int Width = 128;
 	private const int Height = 128;
+	private Timer timer;
 
 	public override void _Ready()
 	{
+		timer = GetParent().GetNode<Timer>("Timer");
+		timer.Connect("timeout", this, nameof(OnTimeout));
+		timer.Autostart = true;
+		// timer.Start(1);
+
 		_rnd = new Random();
 
 		var plane = new PlaneMesh
@@ -40,5 +46,23 @@ public class squares : MultiMeshInstance
 		Multimesh.SetInstanceColor(0, Colors.Purple);
 		Multimesh.SetInstanceColor(1, Colors.Purple);
 		Multimesh.SetInstanceColor(2, Colors.Purple);
+	}
+
+	private void OnTimeout()
+	{
+		var incrVal = 0.1f;
+
+		for (int z = 0; z < Height; z++)
+			for (int x = 0; x < Width; x++)
+			{
+				var index = x + (z * Height);
+				var color = Multimesh.GetInstanceColor(index);
+				var newColor = new Color(
+					color.r = color.r >= 1 ? 0 : color.r + incrVal,
+					color.g = color.g >= 1 ? 0 : color.g + incrVal,
+					color.b = color.b >= 1 ? 0 : color.b + incrVal
+				);
+				Multimesh.SetInstanceColor(index, newColor);
+			}
 	}
 }
